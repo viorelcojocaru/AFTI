1ca15fac302f5d9ccdebbe6af3daa93f56b11050
viorelcojocaru <viorelcojocaru2008@gmail.com>
2016-03-23 16:54:34 +0200

    test

---
+++ test/unitTest.java
@@ -0,0 +1,17 @@
+package test;
+
+/**
+ * Created by viorel.cojocaru on 23.03.2016.
+ */
+import org.junit.Test;
+
+public class unitTest {
+//    public static void main(String[] args) {
+//        System.out.println("unitTest.main"+UNIT_MEASURE);
+//    }
+
+@Test
+    public void should() {
+            //assertThat(assertThat(UNIT_MEASURE.LINIAR_METER.Full()));
+    }
+}
