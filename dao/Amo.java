fc70b34f72cd36857f372221d821f033ccb4e49f
viorelcojocaru <viorelcojocaru2008@gmail.com>
2017-09-22 07:13:42 +0300

    finis anagrams

--- dao/Amo.java
+++ dao/Amo.java
@@ -27,6 +27,86 @@ public class Amo {
       dictionary.put(abet, abet.hashCode());
       dictionary.put("beat", abet.hashCode());
       dictionary.put("beta", abet.hashCode());
+       String abets="abets";
+      dictionary.put(abets, abets.hashCode());
+      dictionary.put("baste", abets.hashCode());
+      dictionary.put("betas", abets.hashCode());
+      dictionary.put("beast", abets.hashCode());
+      dictionary.put("beats", abets.hashCode());
+       String abut="abut";
+      dictionary.put(abut, abut.hashCode());
+      dictionary.put("tabu", abut.hashCode());
+      dictionary.put("tuba", abut.hashCode());
+       String acme="acme";
+      dictionary.put(acme, acme.hashCode());
+      dictionary.put("came", acme.hashCode());
+      dictionary.put("mace", acme.hashCode());
+       String acre="acre";
+      dictionary.put(acre, acre.hashCode());
+      dictionary.put("care", acre.hashCode());
+      dictionary.put("race", acre.hashCode());
+       String acres="acres";
+      dictionary.put(acres, acres.hashCode());
+      dictionary.put("cares", acres.hashCode());
+      dictionary.put("races", acres.hashCode());
+      dictionary.put("scare", acres.hashCode());
+       String actors="actors";
+      dictionary.put(actors, actors.hashCode());
+      dictionary.put("costar", actors.hashCode());
+      dictionary.put("castor", actors.hashCode());
+       String actress="actress";
+      dictionary.put(actress, actress.hashCode());
+      dictionary.put("casters", actress.hashCode());
+      dictionary.put("recasts", actress.hashCode());
+       String airmen="airmen";
+      dictionary.put(airmen, airmen.hashCode());
+      dictionary.put("marine", airmen.hashCode());
+      dictionary.put("remain", airmen.hashCode());
+       String alert="alert";
+      dictionary.put(acme, alert.hashCode());
+      dictionary.put("alter", alert.hashCode());
+      dictionary.put("later", alert.hashCode());
+       String alerted="alerted";
+      dictionary.put(alerted, alerted.hashCode());
+      dictionary.put("altered", alerted.hashCode());
+      dictionary.put("related", alerted.hashCode());
+      dictionary.put("treadle", alerted.hashCode());
+       String ales="ales";
+      dictionary.put(ales, ales.hashCode());
+      dictionary.put("leas", ales.hashCode());
+      dictionary.put("sale", ales.hashCode());
+      dictionary.put("seal", ales.hashCode());
+       String aligned="aligned";
+      dictionary.put(aligned, aligned.hashCode());
+      dictionary.put("dealing", aligned.hashCode());
+      dictionary.put("leading", aligned.hashCode());
+       String allergy="allergy";
+      dictionary.put(allergy, allergy.hashCode()); 
+      dictionary.put("gallery", allergy.hashCode());
+      dictionary.put("largely", allergy.hashCode());
+      dictionary.put("regally", allergy.hashCode());
+       String amen="amen";
+      dictionary.put(amen, amen.hashCode());
+      dictionary.put("amen", amen.hashCode());
+      dictionary.put("mean", amen.hashCode());
+      dictionary.put("name", amen.hashCode());
+            String anew="anew";
+      dictionary.put(anew, anew.hashCode());
+      dictionary.put("wane", anew.hashCode());
+      dictionary.put("wean", anew.hashCode());
+            String angel="angel";
+      dictionary.put(ales, angel.hashCode());
+      dictionary.put("angle", angel.hashCode());
+      dictionary.put("glean", angel.hashCode());
+       String antler="antler";
+      dictionary.put(antler, antler.hashCode());
+      dictionary.put("learnt", antler.hashCode());
+      dictionary.put("rental", antler.hashCode());
+       String apt="apt";
+      dictionary.put(apt, apt.hashCode());
+      dictionary.put("pat", apt.hashCode());
+      dictionary.put("tap", apt.hashCode());
+
      return dictionary;
  }
   private static final Map<Integer,Set<String>> buildDictionarySource(){
@@ -64,7 +144,9 @@ public class Amo {
         }
         return minTo_0;
     }
-
+public boolean isAnagram(String str){
+    return buildDictionary1().containsKey(str);
+}
     public static void main(String[] args) {
         double[] ts={-11,-2,-1,-2,1,3-5,-8, -18};
 //  System.out.print(closerToZero(ts));
